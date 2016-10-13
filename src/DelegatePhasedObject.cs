64ed9aac7db15775d26313a64983ee077a365f88
Shinji Yoshida <yoshida@nurve.jp>
2016-10-13 10:15:26 +0900

    add PhasedObject

--- src/DelegatePhasedObject.cs
+++ src/DelegatePhasedObject.cs
@@ -1,37 +1,29 @@
 ﻿namespace SharpPhase {
 
-	public class DelegatePhasedObject<TDelegator, TPhase> : IPhasedObject<TDelegator, TPhase> where TDelegator : IPhasedObject where TPhase : Phase<TDelegator> {
+	public class DelegatePhasedObject<TDelegator, TPhase>
+		: PhasedObject<TDelegator, TPhase>
+			where TDelegator : IPhasedObject
+			where TPhase : Phase<TDelegator>
+	{
 		TDelegator delegator;
-		TPhase currentPhase;
+
+		protected override TDelegator Self {
+			get {
+				return delegator;
+			}
+		}
 
 		public DelegatePhasedObject(TDelegator delegator, TPhase initialPhase) {
 			this.delegator = delegator;
-			this.currentPhase = initialPhase;
-			initialPhase.EnterPhase(delegator);
-		}
 
-		public void ChangePhase (TPhase newPhase) {
-			if (currentPhase == null)
-				throw new System.Exception ("already terminated");
-			
-			currentPhase.LeavePhase(delegator);
-			currentPhase = newPhase;
-			currentPhase.EnterPhase(delegator);
-		}
-
-		public void TerminatePhase () {
-			currentPhase.TerminatePhase (delegator);
-			currentPhase = null;
-		}
-
-		public TPhase CurrentPhase {
-			get {
-				return currentPhase;
-			}
+			InitPhase (initialPhase);
 		}
 	}
 
-	public class DelegatePhasedObject<TDelegator> : DelegatePhasedObject<TDelegator, Phase<TDelegator>> where TDelegator : IPhasedObject {
+	public class DelegatePhasedObject<TDelegator>
+		: DelegatePhasedObject<TDelegator, Phase<TDelegator>>
+			where TDelegator : IPhasedObject
+	{
 		public DelegatePhasedObject(TDelegator delegator, Phase<TDelegator> initialPhase)
 			: base(delegator, initialPhase)
 		{
---
+++ src/PhasedObject.cs
@@ -0,0 +1,56 @@
+﻿namespace SharpPhase {
+
+	public abstract class PhasedObject<TSelf, TPhase>
+		: IPhasedObject<TSelf, TPhase>
+			where TSelf : IPhasedObject
+		where TPhase : Phase<TSelf>
+	{
+		TPhase currentPhase;
+
+		protected abstract TSelf Self {
+			get;
+		}
+
+		protected PhasedObject(TPhase initialPhase) {
+			InitPhase (initialPhase);
+		}
+
+		/// <summary>
+		/// Should call InitPhase(TPhase) after constructed.
+		/// </summary>
+		protected PhasedObject() {
+		}
+
+		protected void InitPhase(TPhase initialPhase) {
+			this.currentPhase = initialPhase;
+			initialPhase.EnterPhase (Self);
+		}
+
+		public void ChangePhase (TPhase newPhase) {
+			if (currentPhase == null)
+				throw new System.Exception ("already terminated");
+
+			currentPhase.LeavePhase(Self);
+			currentPhase = newPhase;
+			currentPhase.EnterPhase(Self);
+		}
+
+		public void TerminatePhase () {
+			currentPhase.TerminatePhase (Self);
+			currentPhase = null;
+		}
+
+		public TPhase CurrentPhase {
+			get {
+				return currentPhase;
+			}
+		}
+	}
+
+	public abstract class PhasedObject<TSelf> : PhasedObject<TSelf, Phase<TSelf>> where TSelf : IPhasedObject {
+		protected PhasedObject(Phase<TSelf> initialPhase)
+			: base(initialPhase)
+		{
+		}
+	}
+}
